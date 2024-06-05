3dc6c6df0d7be4b50df326ddd382894e36d086ba
пккк <torr95157@gmail.com>
2024-06-06 02:19:05 +0300

    g

---
+++ src/test/java/ru/skypro/calculatorr/CalculatorrApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skypro.calculatorr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
